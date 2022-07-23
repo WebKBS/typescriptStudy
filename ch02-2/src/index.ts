34b962e4ded5680e6d951da6577844e2a6634024
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-23 18:57:22 +0900

    외부 패키지를 불러와서 import 하기

--- ch02-2/src/index.ts
+++ ch02-2/src/index.ts
@@ -1,10 +1,20 @@
-import Person, { makePerson } from "./person/Person";
+// import Person, { makePerson } from "./person/Person";
 import Iperson from "./person/IPerson";
+import Person from "./person/Person";
+import Chance from "chance";
+import * as R from "ramda";
 
-const testMakePerson = (): void => {
-  let jane: Iperson = makePerson("Jane");
-  let jack: Iperson = new Person("Jack");
-  console.log(jane, jack);
-};
+const chance = new Chance();
+let persons: Iperson[] = R.range(0, 2).map(
+  (n: number) => new Person(chance.name(), chance.age())
+);
 
-testMakePerson();
+console.log(persons);
+
+// const testMakePerson = (): void => {
+//   let jane: Iperson = makePerson("Jane");
+//   let jack: Iperson = new Person("Jack");
+//   console.log(jane, jack);
+// };
+
+//testMakePerson();
