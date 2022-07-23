40c7757d06ac88fc75f5f720b0468c4b71b71616
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-23 18:13:25 +0900

    모듈 import * as 사용하기

---
+++ ch02-2/src/person/Person.ts
@@ -0,0 +1,18 @@
+import * as U from "../utils/makeRandomNumber";
+
+export interface Iperson {
+  name: string;
+  age: number;
+}
+
+export class Person implements Iperson {
+  constructor(public name: string, public age: number) {}
+}
+
+export const makePerson = (
+  name: string,
+  age: number = U.makeRandomNumber()
+) => ({
+  name,
+  age,
+});
