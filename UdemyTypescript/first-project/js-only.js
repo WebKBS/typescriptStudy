5491f4ecc63a25eb4c85eefb3a20f7901e0830b6
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-15 21:19:25 +0900

    유데미 typecript 시작

---
+++ UdemyTypescript/first-project/js-only.js
@@ -0,0 +1,18 @@
+// javascript 예시
+
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
+
+function add(num1, num2) {
+  //
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    return +num1 + +num2; // 문자에 +를 앞에 붙으면 number로 바뀐다.
+  }
+}
+
+button.addEventListener("click", function () {
+  console.log(add(input1.value, input2.value));
+});
