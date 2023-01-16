6ceba5aa0766d3bc7795d9a7e83c06911d56f900
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-16 22:59:18 +0900

    타입 추론 알아보기

--- UdemyTypescript/course-starting-project/app.ts
+++ UdemyTypescript/course-starting-project/app.ts
@@ -13,9 +13,13 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   }
 }
 
-const number1 = 5;
+// 변수에 타입을 지정하지 않은 경우 타입을 따로 정해주자
+let number1: number;
+number1 = 5;
+
+// const를 사용했을땐 상수를 변경할 수 없으므로 타입이 추가 되지 않아도 된다.
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = "Result is: ";
+let resultPhrase = "Result is: ";
 
 add(number1, number2, printResult, resultPhrase);
