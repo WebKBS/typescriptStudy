0fe99f8a328b314ef13b929d5008cfc8d8198f76
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-17 23:09:18 +0900

    타입스크립트 배열 사용해보기

--- UdemyTypescript/course-starting-project/app.ts
+++ UdemyTypescript/course-starting-project/app.ts
@@ -1,9 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+// } = {
+const person = {
   name: "Kang",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteAcivities: string[];
+favoriteAcivities = ["Sports"];
+
 console.log(person.name);
+
+for (const bobby of person.hobbies) {
+  console.log(bobby);
+}
