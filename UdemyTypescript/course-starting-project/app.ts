bd104fb5dcdf139fa4c13466ad5f00dbc3dae32a
BeomSu-Kang <kbs21c1987@gmail.com>
2023-01-21 00:06:29 +0900

    타입 앨리어스, type aliase 타입 별칭 사용하기

--- UdemyTypescript/course-starting-project/app.ts
+++ UdemyTypescript/course-starting-project/app.ts
@@ -1,7 +1,11 @@
+// type aliases 타입 별칭 사용
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text" // 투 문자열만 특정하여 지정
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor // 투 문자열만 특정하여 지정
 ) {
   let result;
   if (
