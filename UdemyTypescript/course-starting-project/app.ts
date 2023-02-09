c2af0ae6cbd7166ea7e91b314b994e701b97b726
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-09 21:10:34 +0900

    타입 기능의 함수

--- UdemyTypescript/course-starting-project/app.ts
+++ UdemyTypescript/course-starting-project/app.ts
@@ -2,9 +2,19 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResult(num: number) {
+function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
 printResult(add(5, 12));
+
+//let combineValues: Function;
+
+let combineValues: (a: number, b: number) => number; // return 값도 number가 나오도록
+
+combineValues = add;
+combineValues = printResult;
+
+console.log(combineValues(8, 8));
+
 //console.log(printResult(add(5, 12))); // undefined
