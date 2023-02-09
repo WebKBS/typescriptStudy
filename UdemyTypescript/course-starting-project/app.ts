ee0fb857ae8e07deb3c2897be287ef0c169e29e4
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-09 21:21:37 +0900

    함수 콜백 타입 지정하기

--- UdemyTypescript/course-starting-project/app.ts
+++ UdemyTypescript/course-starting-project/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+} // void는 아무 작업도 수행하지 않는다.
+
 printResult(add(5, 12));
 
 //let combineValues: Function;
@@ -13,8 +18,13 @@ printResult(add(5, 12));
 let combineValues: (a: number, b: number) => number; // return 값도 number가 나오도록
 
 combineValues = add;
-combineValues = printResult;
+//combineValues = printResult;
 
 console.log(combineValues(8, 8));
 
 //console.log(printResult(add(5, 12))); // undefined
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return result;
+});
