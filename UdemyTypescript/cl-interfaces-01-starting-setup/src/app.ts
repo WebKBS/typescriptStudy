36ab1e1d4b182abbc2b53d54ac3ee0d21b018cd0
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-17 22:59:26 +0900

    인터페이스 클래스와 관계 알아보기

--- UdemyTypescript/cl-interfaces-01-starting-setup/src/app.ts
+++ UdemyTypescript/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,22 +1,31 @@
-interface Person {
+interface Greetable {
   // 대문자로. 관례임
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-  name: "Kang",
-  age: 123,
+  constructor(n: string) {
+    this.name = n;
+  }
 
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Kang");
 
 user1.greet("Hi there - I am");
 
 console.log(user1);
+
+// 인터페이스를 쓰는 이유.
+// interface대신 type을 사용해도 작동하긴 하나,
+// 인터페이스는 객체 구조를 설명하기 위해서만 사용한다.
