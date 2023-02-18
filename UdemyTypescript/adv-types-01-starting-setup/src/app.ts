7f723ccc2fc1202a90a702dca5480e8aca4c5ee0
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-18 22:15:46 +0900

    타입 가드 사용하기

--- UdemyTypescript/adv-types-01-starting-setup/src/app.ts
+++ UdemyTypescript/adv-types-01-starting-setup/src/app.ts
@@ -21,3 +21,59 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// 함수 타입 가드 ==================
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+console.log(add("1", "4"));
+
+type UnknowEmoloyee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknowEmoloyee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "Menu", startDate: new Date() });
+
+// 클래스 타입가드
+class Car {
+  drive() {
+    console.log("Driving..");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Drive Truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) { //instanceof (js 내장메서드) 사용해서 조건 검사
+    vehicle.loadCargo(1000);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
