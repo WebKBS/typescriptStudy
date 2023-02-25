9050c1e3fcd0c5200edc444ab53d56ba96ecac35
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-25 22:40:21 +0900

    제네릭 시작

---
+++ UdemyTypescript/generics-01-starting-setup/src/app.ts
@@ -0,0 +1,13 @@
+//const names = ["Kang", "Manuel"];
+const names: Array<string> = [];
+// names[0].split(' ');
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
+});
+
+promise.then((data) => {
+  data.split(" ");
+});
