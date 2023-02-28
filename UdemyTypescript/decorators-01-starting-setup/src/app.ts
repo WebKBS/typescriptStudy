20976afd23b63bb79cb812868d70346f9c7fd0c3
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-28 23:34:29 +0900

    다중 데코레이터 사용. 데코레이터 함수실행은 아래에서부터 시작한다.

--- UdemyTypescript/decorators-01-starting-setup/src/app.ts
+++ UdemyTypescript/decorators-01-starting-setup/src/app.ts
@@ -1,6 +1,7 @@
 // 데코레이터를 사용하려면 tsconfig.json에서 experimentalDecorators를 활성화 해야한다.
 
 function Logger(logString: string) {
+  console.log("Logger Factory");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -8,6 +9,7 @@ function Logger(logString: string) {
 }
 
 function WithTemplete(templete: string, hookId: string) {
+  console.log("Templete Factory");
   return function (constructor: any) {
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
@@ -21,7 +23,8 @@ function WithTemplete(templete: string, hookId: string) {
 
 // 데코레이터는 인수를 받는다.
 //@Logger("Loggin - Person")
-@WithTemplete("<h1>My Person Object </h1>", "app")
+@Logger("Logger")
+@WithTemplete("<h1>My Person Object </h1>", "app") // 다중 데코레이터 함수 실행은 아래부터 시작한다.
 class Person {
   name = "Kang";
 
