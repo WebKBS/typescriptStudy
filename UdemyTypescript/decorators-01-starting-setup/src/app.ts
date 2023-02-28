a3571ea120dc01bb03ce3d90871dd57592a164f4
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-28 23:44:17 +0900

    속성 데코레이터 알아보기

--- UdemyTypescript/decorators-01-starting-setup/src/app.ts
+++ UdemyTypescript/decorators-01-starting-setup/src/app.ts
@@ -36,3 +36,34 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+///// -------
+
+// 데코레이터
+function Log(target: any, propertyName: string | symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("price가 다릅니다.");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
