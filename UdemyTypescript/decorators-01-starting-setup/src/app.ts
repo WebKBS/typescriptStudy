39cb45176af4aa16084c8d45adfca5514a24a293
BeomSu-Kang <kbs21c1987@gmail.com>
2023-02-28 22:36:56 +0900

    데코레이터 인수 전달

--- UdemyTypescript/decorators-01-starting-setup/src/app.ts
+++ UdemyTypescript/decorators-01-starting-setup/src/app.ts
@@ -1,13 +1,14 @@
 // 데코레이터를 사용하려면 tsconfig.json에서 experimentalDecorators를 활성화 해야한다.
 
-function Logger(constructor: Function) {
-  console.log("logging...");
-  console.log(constructor);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
 // 데코레이터는 인수를 받는다.
-@Logger
-
+@Logger("Loggin - Person")
 class Person {
   name = "Kang";
 
