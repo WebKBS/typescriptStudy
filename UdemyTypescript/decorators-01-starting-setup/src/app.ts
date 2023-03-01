8c14394f6f43affdd48115f6fe59dfd2e92b51c4
BeomSu-Kang <kbs21c1987@gmail.com>
2023-03-01 21:49:32 +0900

    class  autoBind 만들기

--- UdemyTypescript/decorators-01-starting-setup/src/app.ts
+++ UdemyTypescript/decorators-01-starting-setup/src/app.ts
@@ -105,3 +105,32 @@ class Product {
 
 const p1 = new Product("book", 19);
 const p2 = new Product("book", 29);
+
+////// -------
+
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works";
+
+  @autoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
