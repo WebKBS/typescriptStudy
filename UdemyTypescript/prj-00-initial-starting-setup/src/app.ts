95c3951a734eefab0443a8a499e25575a600b6c5
BeomSu-Kang <kbs21c1987@gmail.com>
2023-03-07 21:34:17 +0900

    랜더링 프로젝트

--- UdemyTypescript/prj-00-initial-starting-setup/src/app.ts
+++ UdemyTypescript/prj-00-initial-starting-setup/src/app.ts
@@ -61,6 +61,41 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   //tsconfig json에서 experimentalDecorators 를 활성화 해야함.
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = <HTMLDivElement>document.getElementById("app")!;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + "PROJECT";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 // ProjectInput class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -160,3 +195,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishPrjList = new ProjectList("finished");
