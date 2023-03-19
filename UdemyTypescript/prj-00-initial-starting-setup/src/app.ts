d80755bb50db7ff5e2337101cb6af8427b5e447c
BeomSu-Kang <kbs21c1987@gmail.com>
2023-03-19 17:57:10 +0900

    네임스페이스 필요한 파일만 import하기

--- UdemyTypescript/prj-00-initial-starting-setup/src/app.ts
+++ UdemyTypescript/prj-00-initial-starting-setup/src/app.ts
@@ -1,9 +1,5 @@
 // /// 슬러시 3개내는 타입스크립트의 특별한 주석
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
+
 /// <reference path="components/project-input.ts" />
 /// <reference path="components/project-list.ts" />
 
