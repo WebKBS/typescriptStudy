63e32268c9936370102b8baeb46ebb240c656292
BeomSu-Kang <kbs21c1987@gmail.com>
2023-03-23 23:42:39 +0900

    .

--- UdemyTypescript/prj-00-initial-starting-setup/webpack.config.prod.js
+++ UdemyTypescript/prj-00-initial-starting-setup/webpack.config.prod.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -23,4 +24,6 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
