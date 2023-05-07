a9ba04aedc90ae813b65c2e94e4b6f298d2d4250
BeomSu-Kang <kbs21c1987@gmail.com>
2023-05-07 23:11:30 +0900

    React - Typescript

---
+++ UdemyTypescript/react-typescript/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+
+function App() {
+  const todos = [{ id: "t1", text: "첫번째 텍스트" }];
+
+  return (
+    <div className="App">
+      <NewTodo />
+      <TodoList items={todos} />
+    </div>
+  );
+}
+
+export default App;
